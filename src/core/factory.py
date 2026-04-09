f039bc185bdd4d2f04d736794457ef304ed35230
XuRuntian <xuruntian03@163.com>
2026-04-09 15:03:15 +0800

    merge master

--- src/core/factory.py
+++ src/core/factory.py
@@ -82,7 +82,7 @@ class ReaderFactory:
         
         # 先尝试匹配具体业务 (如 ZhiPingFang)
         for profile_name, data in all_rules.items():
-            if ReaderFactory._evaluate_rules(path, data.get("match_rules", {})):
+            if isinstance(data, dict) and ReaderFactory._evaluate_rules(path, data.get("match_rules", {})):
                 config_dict = data
                 break
                 
